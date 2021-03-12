09341ff4d8167cbb4c6fdee929a96b62c196fd59
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-12 13:43:33 +0100

    Created ex03

---
+++ ex03/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // Only change code below this line 
+
+    // Only change code above this line
+
+    var myString = "\nFirstLine\n\tSecondLine\nThirdLine";
+
+    return myString;
+}
+console.log(main());
+module.exports =main;
