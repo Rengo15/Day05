c17994736f9c3d2f7012e14863d45074dc180cd8
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-12 12:52:53 +0100

    Created ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // Only change code below this line
+
+    // Only change code above this line
+    var myFirstString = "I think therefore I \"am\" -- sentient.";
+    var mySecondString = "No, you only \"think\", so you aren't.";
+    return {
+        myFirstString,
+        mySecondString
+
+    };
+
+}
+console.log(main());
+module.exports = main();
