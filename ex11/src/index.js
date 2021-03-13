e64f01fd63b939a103aa57d435fa974762b77375
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-13 10:18:20 +0100

    Created ex11

---
+++ ex11/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // Only change code below this line
+
+    // Only change code above this line
+
+
+    var lastName = "Tesla";
+    var thirdLetterOfLastName =lastName[2];
+    
+
+    return thirdLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
