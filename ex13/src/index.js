d470637c6fb1dc704bcfb284a12ae668bbf404fa
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-13 10:27:17 +0100

    Created ex13

---
+++ ex13/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // Only change code below this line
+
+    // Only change code above this line
+
+
+    var lastName = "Bezos";
+    var thirdTolastLetterOfLastName = lastName[lastName.length - 3];
+    
+
+    return thirdTolastLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
