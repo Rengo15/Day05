b23a4a7a536fde0e85a3bf42bb2a2ad0a4d4da80
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-13 10:14:20 +0100

    Created ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // Only change code below this line
+
+    // Only change code above this line
+
+
+    var myString ="Jello World";
+    myString ="Hello World";
+
+    return myString;
+}
+console.log(main());
+module.exports = main;
