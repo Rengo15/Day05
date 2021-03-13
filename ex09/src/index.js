6a7f5f9cda653751500bc13b22a2b08ffda6054a
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-13 10:02:07 +0100

    Created ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // Only change code below this line
+
+    // Only change code above this line
+
+
+    var lastName = "Doe";
+    var firstLetterOfLastName = " ";
+    firstLetterOfLastName = lastName[0];
+
+    return firstLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
