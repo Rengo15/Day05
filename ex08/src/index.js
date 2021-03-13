ebff399573697d8659721404e44729ef4663baad
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-13 09:56:11 +0100

    Created ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // Only change code below this line
+
+    // Only change code above this line
+
+
+    var lastName = "Doe";
+    var lastNameLength;
+    lastNameLength = lastName.length;
+
+    return lastNameLength;
+}
+console.log(main());
+module.exports = main;
