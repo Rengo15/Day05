202ac717b9ec3bb5da0253a5cb6d019f9dee973d
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-13 10:46:44 +0100

    Created ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    // Only change code below this line
+
+    // Only change code above this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective ="big";
+    var myAdverb = "quickly";
+    var wordBlanks = "";
+    wordBlanks=myAdjective +  " " + myNoun + " " + myVerb + " " + myAdverb +".";
+
+
+    return wordBlanks;
+}
+console.log(main())
+module.exports=main;
